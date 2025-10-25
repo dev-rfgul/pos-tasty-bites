1c4da36e934a442865580857ff1246f0cba8329b
dev-rfgul <rfgul587@gmail.com>
2025-10-25 17:23:36 +0500

    initialized the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-red-600
+    '>
+      <h1 className='text-white'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
